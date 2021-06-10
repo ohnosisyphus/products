abec99e9d9ea293a34302d2c981a5de1f16239f8
ohnosisyphus <ohnosisyphus@gmail.com>
2021-06-10 15:41:20 +0800

    add output csv

--- products.py
+++ products.py
@@ -10,3 +10,8 @@ print(products)
 for p in products:
 	#print(p[0]) #只印出名字
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價錢\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
